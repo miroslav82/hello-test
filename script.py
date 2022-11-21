ef2c379d9682c2471d0bae30d13df42a1eef7c87
Tripko <miroslav82@yahoo.com>
2022-11-21 22:23:42 +0100

    Delete script.py

--- script.py
+++
@@ -1,9 +0,0 @@
-n = [[1, 2, 3], [4, 5, 6, 7, 8, 9]]
-
-def flatten (lists):
-    result = []
-    for i in range(len(lists)):
-        for j in range(len(lists[i])):
-            result.append(lists[i][j])
-    return result
-print flatten(n)
